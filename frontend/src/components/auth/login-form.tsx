dd168d6f94377f06c0f00c8dbbd74f3b187690e8
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2026-01-20 12:07:28 +0500

    build error resolved

--- frontend/src/components/auth/login-form.tsx
+++ frontend/src/components/auth/login-form.tsx
@@ -1,3 +1,4 @@
+{/* eslint-disable-next-line react/no-unescaped-entities */}
 'use client';
 
 import React, { useState } from 'react';
@@ -91,7 +92,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ onLoginSuccess, onSwitchToRegiste
 
       <div className="mt-4 text-center">
         <p className="text-[#a0aec0]">
-          Don&apos;t have an account?
+           {"Don't have an account?"}
           <button
             onClick={onSwitchToRegister}
             className="text-blue-400 hover:text-blue-300 hover:underline focus:outline-none transition-colors"
